a540bb44c17f2093a248a85c8204f3d783ec3b66
cempa96 <132065695+cempa96@users.noreply.github.com>
2025-06-18 08:55:20 +0200

    added max warning limit to lint checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginSecurity from "eslint-plugin-security";
 import { defineConfig } from "eslint/config";
 
 
@@ -8,4 +9,5 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended
 ]);
